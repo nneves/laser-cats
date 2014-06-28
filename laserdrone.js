ea3a5f7a35751081a87be4cdc13fc1012f2562b3
Nelson Neves <nelson.s.neves@gmail.com>
2014-06-28 19:14:14 +0100

    Initial module laserdrone.js import

---
+++ laserdrone.js
@@ -0,0 +1,20 @@
+module.exports = {
+	up: function () { 
+		console.log(arguments);
+	},
+	down: function () {
+		console.log(arguments);
+	},
+	left: function () {
+		console.log(arguments);
+	},
+	right: function () {
+		console.log(arguments);
+	},
+	fire: function () {
+		console.log(arguments);
+	},
+	toggle: function () {
+		console.log(arguments);
+	}
+}
