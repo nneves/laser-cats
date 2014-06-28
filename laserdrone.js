728493c4124217215ab36acbc8c958636365acc8
Nelson Neves <nelson.s.neves@gmail.com>
2014-06-28 19:55:21 +0100

    connected the rest of the functionality

--- laserdrone.js
+++ laserdrone.js
@@ -1,20 +1,62 @@
+var five = require("johnny-five");
+
+// local tilt/rotation position variables
+var tiltPos = 90;
+var rotationPos = 90;
+
+var tiltDelta = 10;
+var rotationDelta = 10;
+
+var servo1;
+var servo2;
+var laser;
+var laserStatus = false;
+
+	var config1 = {
+    	pin: 10,
+    	startAt: tiltPos
+    };
+
+	var config2 = {
+	    pin: 11,
+	    startAt: rotationPos
+	};
+
+	var config3 = {
+	    pin: 8	    
+	};
+
+five.Board().on("ready", function() {
+
+	console.log("Connected");
+
+
+
+	// Initialize the servo
+	servo1 = new five.Servo(config1);
+	servo2 = new five.Servo(config2);
+	laser = new five.Led(config3);
+});
+
 module.exports = {
-	up: function () { 
-		console.log(arguments);
+	down: function () { 
+		servo1.step(tiltDelta);
 	},
-	down: function () {
-		console.log(arguments);
+	up: function () {
+		servo1.step(-1*tiltDelta);
 	},
 	left: function () {
-		console.log(arguments);
+		servo2.step(rotationDelta);
 	},
 	right: function () {
-		console.log(arguments);
+		console.log(-1*rotationDelta);
 	},
-	fire: function () {
-		console.log(arguments);
-	},
+	// fire: function () {
+	// 	console.log(arguments);
+	// },
 	toggle: function () {
-		console.log(arguments);
+		laserStatus = !laserStatus;
+		if (laserStatus) { laser.on();}
+		else { laser.off(); }
 	}
 }
