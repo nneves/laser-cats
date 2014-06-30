1c1412edc9351bee79b1b6221a370ad675c72585
Ricardo Soares <botequilha@gmail.com>
2014-06-30 19:14:39 +0100

    Added .gitignore
    Adds Q for promises capability
    
    App now uses promises. This fixes issue #1. Also decreases tilt/rotation delta.

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 var bot = require("./laserdrone");
 var keypress = require('keypress');
 
-
-
 keypress(process.stdin);
 
+bot.then( function( commands ) {
+  process.stdin.on('keypress', function (ch, key) {
 
-process.stdin.on('keypress', function (ch, key) {
-  console.log('got "keypress"', key);
+    if (key && key.ctrl && key.name == 'c') {
+      process.stdin.resume();
+      process.stdin.pause();
+    }
 
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
-  }
+    switch( key.name ) {
+      case "q":
+        commands.up();
+        break;
+      case "a":
+        commands.down();
+        break;
+      case "o":
+        commands.left();
+        break;
+      case "p":
+        commands.right();
+        break;
+      case "b":
+        commands.toggle();
+        break;
+      default:
+        console.log(key.name);
+        break;
+    }
+  });
 
-  switch( key.name ) {
-    case "q":
-      bot.up();
-      break;
-    case "a":
-      bot.down();
-      break;
-    case "o":
-      bot.left();
-      break;
-    case "p":
-      bot.right();
-      break;
-    case "b":
-      bot.toggle();
-      break;
-    default:
-      console.log(key.name);
-      break;
-  }
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
 });
-
-process.stdin.setRawMode(true);
-process.stdin.resume();
--- laserdrone.js
+++ laserdrone.js
@@ -1,62 +1,68 @@
-var five = require("johnny-five");
+var five = require("johnny-five"),
+		Q = require("q");
 
 // local tilt/rotation position variables
 var tiltPos = 90;
 var rotationPos = 90;
 
-var tiltDelta = 10;
-var rotationDelta = 10;
+var tiltDelta = 2;
+var rotationDelta = 2;
 
 var servo1;
 var servo2;
 var laser;
 var laserStatus = false;
 
-	var config1 = {
-    	pin: 10,
-    	startAt: tiltPos
-    };
+var config1 = {
+  	pin: 10,
+  	startAt: tiltPos
+  };
 
-	var config2 = {
-	    pin: 11,
-	    startAt: rotationPos
-	};
+var config2 = {
+    pin: 11,
+    startAt: rotationPos
+};
 
-	var config3 = {
-	    pin: 8	    
-	};
+var config3 = {
+    pin: 8
+};
 
-five.Board().on("ready", function() {
+// Deferred object
+var deferred = Q.defer();
 
-	console.log("Connected");
+function init() {
+	var commands = {};
 
+	five.Board().on("ready", function() {
+		// Initialize the servo
+		servo1 = new five.Servo(config1);
+		servo2 = new five.Servo(config2);
+		laser = new five.Led(config3);
 
+		// Setup commands object
+		commands = {
+			down: function () {
+				servo1.step(tiltDelta);
+			},
+			up: function () {
+				servo1.step(-1*tiltDelta);
+			},
+			left: function () {
+				servo2.step(rotationDelta);
+			},
+			right: function () {
+				servo2.step(-1*rotationDelta);
+			},
+			toggle: function () {
+				laserStatus = !laserStatus;
+				if (laserStatus) { laser.on();}
+				else { laser.off(); }
+			}
+		};
 
-	// Initialize the servo
-	servo1 = new five.Servo(config1);
-	servo2 = new five.Servo(config2);
-	laser = new five.Led(config3);
-});
-
-module.exports = {
-	down: function () { 
-		servo1.step(tiltDelta);
-	},
-	up: function () {
-		servo1.step(-1*tiltDelta);
-	},
-	left: function () {
-		servo2.step(rotationDelta);
-	},
-	right: function () {
-		console.log(-1*rotationDelta);
-	},
-	// fire: function () {
-	// 	console.log(arguments);
-	// },
-	toggle: function () {
-		laserStatus = !laserStatus;
-		if (laserStatus) { laser.on();}
-		else { laser.off(); }
-	}
+		deferred.resolve(commands) ;
+	});
+	return deferred.promise;
 }
+
+module.exports = init();
